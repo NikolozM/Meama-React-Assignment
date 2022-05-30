62193e4c7401ba7070bb9bb17b506568a63f71fb
Nikoloz <mazmishvilinika888@gmail.com>
2022-05-30 16:45:55 +0400

    ada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  return <h1>Welcome to Meama</h1>;
+  return (
+  <div>
+  
+  <h1>Welcome to Meama</h1>
+
+  <h2>adad</h2>
+  </div>
+  )
+
 }
 
 export default App;
